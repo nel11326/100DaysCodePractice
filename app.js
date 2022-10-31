85ea9897bad7a899cdc50820d77143507213058a
Ronel Tayawa <lenor11326@hotmail.com>
2022-10-31 18:27:35 +0800

    Day 34 challenge complete

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-document.body.children[1].children[0].href = "https://google.com";
+// document.body.children[1].children[0].href = "https://google.com";
+
+let anchorElement = document.getElementById("external-link");
+anchorElement.href = "https://google.com";
+
+anchorElement = document.querySelector("p a");
+anchorElement.href = "https://academind.com";
+
+// Add an element
+// 1. create the new element
+let newAnchorElement = document.createElement("a");
+newAnchorElement.href = "https://google.com";
+newAnchorElement.textContent = "This leads to Google";
+// 2. get access to the parent element that should hold the new element
+let firstParagraph = document.querySelector("p");
+// 3. insert the new element into the parent element content
+firstParagraph.append(newAnchorElement);
