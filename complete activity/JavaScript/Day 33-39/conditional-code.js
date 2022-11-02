06fcc4a9cb2f5cba8909ad0d9cd976d0dcce848d
Ronel Tayawa <lenor11326@hotmail.com>
2022-11-02 19:59:07 +0800

    Day 38 challenge complete

--- complete activity/JavaScript/Day 33-39/conditional-code.js
+++ complete activity/JavaScript/Day 33-39/conditional-code.js
@@ -9,3 +9,11 @@ const isLoggedIn = true;
 if (!isLoggedIn) {
   console.log("User is NOT logged in!");
 }
+
+const enteredUserName = "";
+
+if (enteredUserName.length > 0) {
+  console.log("Input is valid");
+} else {
+  console.log(false);
+}
