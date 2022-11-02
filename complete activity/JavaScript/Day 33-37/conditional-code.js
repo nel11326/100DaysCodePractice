8629853419458f29c3122f3388b7e8b046316f8e
Ronel Tayawa <lenor11326@hotmail.com>
2022-11-02 14:12:49 +0800

    Day 37 Challenge Complete

---
+++ complete activity/JavaScript/Day 33-37/conditional-code.js
@@ -0,0 +1,11 @@
+const myName = "Max";
+
+if (myName === "Max") {
+  console.log("Hello!");
+}
+
+const isLoggedIn = true;
+
+if (!isLoggedIn) {
+  console.log("User is NOT logged in!");
+}
--- complete activity/JavaScript/Day 33-37/demo.js
+++ complete activity/JavaScript/Day 33-37/demo.js
@@ -1,17 +1,30 @@
-let productNameInputElement = document.getElementById("product-name");
-let remainingCharsElement = document.getElementById("remaining-chars");
+const productNameInputElement = document.getElementById("product-name");
+const remainingCharsElement = document.getElementById("remaining-chars");
 
 // console.dir(productNameInputElement);
 
-let maxAllowedChars = productNameInputElement.maxLength;
+const maxAllowedChars = productNameInputElement.maxLength;
 
 function updateRemainingCharacters(event) {
-  let enteredText = event.target.value;
-  let enteredTextLength = enteredText.length;
+  const enteredText = event.target.value;
+  const enteredTextLength = enteredText.length;
 
-  let remainingCharacters = maxAllowedChars - enteredTextLength;
+  const remainingCharacters = maxAllowedChars - enteredTextLength;
 
   remainingCharsElement.textContent = remainingCharacters;
+
+  if (remainingCharacters === 0) {
+    remainingCharsElement.classList.add("error");
+    productNameInputElement.classList.add("error");
+  } else if (remainingCharacters <= 10) {
+    remainingCharsElement.classList.add("warning");
+    productNameInputElement.classList.add("warning");
+    remainingCharsElement.classList.remove("error");
+    productNameInputElement.classList.remove("error");
+  } else {
+    remainingCharsElement.classList.remove("warning");
+    productNameInputElement.classList.remove("warning");
+  }
 }
 
 productNameInputElement.addEventListener("input", updateRemainingCharacters);
