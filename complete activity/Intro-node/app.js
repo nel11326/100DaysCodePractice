b72e44229923cdae95ac36f23646a3b465526a6a
Ronel Tayawa <lenor11326@hotmail.com>
2022-11-07 21:39:39 +0800

    Day 48 challenge Intro to node with express complete

--- complete activity/Intro-node/app.js
+++ complete activity/Intro-node/app.js
@@ -1,15 +1,34 @@
-const http = require("http");
+const fs = require("fs");
+const path = require("path");
 
-function handleRequest(request, response) {
-  if (request.url === "/currenttime") {
-    response.statusCode = 200;
-    response.end("<h1>" + new Date().toISOString() + "</h1>");
-  } else if (request.url === "/") {
-    response.statusCode = 200;
-    response.end("<h1>Hello World!</h1>");
-  }
-}
+const express = require("express");
 
-const server = http.createServer(handleRequest);
+const app = express();
 
-server.listen(3000);
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/currenttime", function (req, res, next) {
+  res.send("<h1>" + new Date().toISOString() + "</h1>");
+});
+
+app.get("/", function (req, res, next) {
+  res.send(
+    '<form action="/store-user" method="POST"><label>Your Name</label><input type="text" name="username"><button>Submit</button></form>'
+  );
+});
+
+app.post("/store-user", function (req, res, next) {
+  const userName = req.body.username;
+
+  const filePath = path.join(__dirname, "data", "users.json");
+
+  const fileData = fs.readFileSync(filePath);
+  const existingUsers = JSON.parse(fileData);
+
+  existingUsers.push(userName);
+
+  fs.writeFileSync(filePath, JSON.stringify(existingUsers));
+  res.send("<h1>Username stored!</h1>");
+});
+
+app.listen(3000);
